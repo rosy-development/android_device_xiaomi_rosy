f5fc50ffc9df6a1adafa42f1c7d0d83e8501ae45
Abhishek Aggarwal <warabhishek@gmail.com>
2019-08-08 13:20:45 +0000

    rosy: Fix DT2W
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -124,6 +124,7 @@ TARGET_PER_MGR_ENABLED := true
 # Power
 TARGET_HAS_NO_WLAN_STATS := true
 TARGET_USES_INTERACTION_BOOST := false
+TARGET_TAP_TO_WAKE_EVENT_NODE := "/dev/input/event2"
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
--- device.mk
+++ device.mk
@@ -280,7 +280,8 @@ PRODUCT_PACKAGES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-pa
 
 # RenderScript
 PRODUCT_PACKAGES += \

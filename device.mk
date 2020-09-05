9ab0f576bf45e7ce2c88f810186d39c38313bfb7
Vishalcj17 <vishalcj@aospa.co>
2020-09-05 09:10:36 +0530

    rosy: update rootdir from LA.UM.8.6.2.r1-08400-89xx.0
    Imported usb.config file too
    
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>

--- device.mk
+++ device.mk
@@ -279,6 +279,7 @@ PRODUCT_PACKAGES += \
     init.recovery.qcom.rc \
     init.recovery.qcom.usb.rc \
     init.rosy.rc \
+    init.msm.usb.configfs.rc \
     init.target.rc \
     ueventd.qcom.rc
 
